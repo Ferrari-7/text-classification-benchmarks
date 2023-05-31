8f1f68b1d9f05585c81574c27292e1dee5d1ca2a
Ferrari-7 <mariamunksoe@gmail.com>
2023-05-31 09:31:16 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# text-classification-benchmarks
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10405356&assignment_repo_type=AssignmentRepo)
+# Assignment 2 - Text classification benchmarks
+
+This repository contains code which will use ```scikit-learn``` to train binary classification models on a dataset of news articles labelled as either fake and real. The dataset can be found found on Kaggle via this [link](https://www.kaggle.com/datasets/jillanisofttech/fake-or-real-news)
+
+This repository contains *two different scripts*. One script trains a logistic regression classifier on the data; the second script trains a neural network on the same dataset. Both scripts does the following:
+
+- Saves a classification report to the folder called ```out```
+- Saves the trained model and vectorizer to the folder called ```models```
+
+
+## User instructions
+
+1. Install necessary packages using the setup script like so: 
+
+```bash setup.sh```
+
+2. Run the script which trains the logistic regression classifier by navigating to the ```src``` folder:
+
+```python logreg.py```
+
+3. Run the script which trains the neural network by navigating to the ```src``` folder: 
+
+```python mlp_clf.py```
+
+
+## Discussion of results
+
+This is the classification report for the **logistic regression classifier**: 
+
+                precision    recall  f1-score   support
+
+    FAKE            0.86      0.74      0.79       628
+    REAL            0.78      0.88      0.82       639
+
+    accuracy                            0.81      1267
+    macro avg       0.82      0.81      0.81      1267
+    weighted avg    0.81      0.81      0.81      1267
+
+And for the **neural network**: 
+
+              precision    recall  f1-score   support
+
+    FAKE            0.86      0.73      0.79       628
+    REAL            0.77      0.88      0.82       639
+
+    accuracy                            0.81      1267
+    macro avg       0.81      0.80      0.80      1267
+    weighted avg    0.81      0.81      0.80      1267
+
+To my initial surprise, the logistic regression classifier seem to be outperforming the multilayer perceptron classifier (although just narrowly). But perhaps this makes sense since logistic regression is particularly suited for binary classification. The weighed avg for the f1-scores is just .01 higher for the logistic regression classifier. 
+
+When I used the same type of models on image data with multible categories, the f1-scores were more than half as low. But these more simple classification models seem to work well on binary classification of text data. 
+
---
+++ src/logreg.py
@@ -0,0 +1,78 @@
+# Import packages
+# system tools
+import os
+import sys
+sys.path.append("..")
+# data munging tools
+import pandas as pd
+import utils.classifier_utils as clf
+# Machine learning tools
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, ShuffleSplit
+from sklearn import metrics
+# for saving models
+from joblib import dump, load
+
+
+# load data
+def load_data():
+    filename = os.path.join("..", "in", "fake_or_real_news.csv")
+    data = pd.read_csv(filename)
+
+    # get text and labels
+    X = data["text"]
+    y = data["label"]
+    # train test split
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.2,
+                                                        random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def doc_vectorizer(X_train, X_test):
+    # vectorizer
+    vectorizer = TfidfVectorizer(ngram_range = (1,2),    # unigrams and bigrams (1 word and 2 word units)
+                                lowercase =  True,       # lowercase
+                                max_df = 0.95,           # remove very common words
+                                min_df = 0.05,           # remove very rare words
+                                max_features = 100)      # keep only top 100 features
+    
+    # fit and transform data to training data
+    X_train_feats = vectorizer.fit_transform(X_train)
+    # fit and transform data to test data
+    X_test_feats = vectorizer.fit_transform(X_test)
+
+    return  X_train_feats, X_test_feats
+
+def lr_model(X_train_feats, X_test_feats, y_train):      
+    # initiaze classifier
+    classifier = LogisticRegression(random_state=42).fit(X_train_feats, y_train)
+
+    # get predictions
+    y_pred = classifier.predict(X_test_feats)
+
+    # save model in folder called "models"
+    clf_name = os.path.join("..", "models", "LR_classifier.joblib")
+    dump(classifier, clf_name)
+
+    return y_pred
+
+def clf_report(y_test, y_pred):
+    # making a classification report
+    classifier_metrics = metrics.classification_report(y_test, y_pred)
+    
+    # saving the report as a txt file
+    report_path = os.path.join("..", "out", "logreg_report.txt")
+    text_file = open(report_path, "w")
+    text_file.write(classifier_metrics)
+    text_file.close()
+
+def main():
+    X_train, X_test, y_train, y_test = load_data()
+    X_train_feats, X_test_feats = doc_vectorizer(X_train, X_test)
+    y_pred = lr_model(X_train_feats, X_test_feats, y_train)
+    clf_report(y_test, y_pred)
+
+if __name__=="__main__":
+    main()
